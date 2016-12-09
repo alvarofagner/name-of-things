fa92a3ff594eb31e6ddb76d4a7ef67d377fa79fa
Alvaro Silva <alvaro.fagner@gmail.com>
2016-12-09 14:39:38 +0100

    Initial commit with the demo shown at InfoVista

---
+++ Assets/HideEverything.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideEverything : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Hidden(bool isHidden)
+	{
+		Renderer[] renderers = gameObject.GetComponentsInChildren<Renderer>();
+		foreach (Renderer r in renderers)
+		{
+			r.enabled = !isHidden;
+		}
+	}
+}
